8560d0b01106804c07fa62e3010ce88d5c699c86
Aaron Lacerda - [Nightdev4l] <122033083+shviam-2018@users.noreply.github.com>
2025-11-23 21:21:57 +0100

    fedig gjort api ved hjelp av tutorial også la til postman cli test også bruker newman for å kjøre den i cli

--- studentapi/src/v1/Controllers/studentController.js
+++ studentapi/src/v1/Controllers/studentController.js
@@ -1,9 +1,14 @@
-const { route } = require('../Routes/studentRoutes');
-const {getStudents, getStudentById, createStudent, updateStudent, deleteStudent} = require('../Services/studentService');
+const {
+    getAllObjects,
+    addObject,
+    getObjectById,
+    deleteObjectById,
+    updateObjectById
+} = require('../Data/DB_Generic');
 
 const getAllStudents = async (req, res) => {
     try {
-        const students = await getStudents();
+        const students = await getAllObjects();
         res.status(200).json({sucess: true, data: students});
     } catch (error) {
         res.status(500).json({sucess: false, error: 'Server Error' });
@@ -12,7 +17,7 @@ const getAllStudents = async (req, res) => {
 
 const getSingleStudent = async (req, res) => {
     try {
-        const student = await getStudentById(req.params.id);
+        const student = await getObjectById(req.params.id);
         res.status(200).json({sucess: true, data: student});
         if (!student) { /*gir en status 404 hvis studenten ikke finnes, jeg legger inn en sjekk for 404 fordi den er den 
 err som jeg har størst mulighet for å få*/
@@ -21,4 +26,43 @@ err som jeg har størst mulighet for å få*/
     } catch (error) {
         res.status(500).json({sucess: false, error: 'Server Error' });
     }
+};
+
+const createStudentObject = async (req, res) => {
+    try {
+        const {name, age} = req.body;
+        const newStudent = await addObject({name, age});
+        res.status(201).json({sucess: true, data: newStudent});
+    } catch (error) {
+        res.status(500).json({sucess: false, error: 'Server Error' });
+    }
+};
+
+const updateStudentObject = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, age } = req.body;
+        const updateStudent = await updateObjectById(id, { name, age });
+        res.status(200).json({ success: true, data: updateStudent });
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Server Error' });
+    }
+};
+
+const deleteStudentObject = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await deleteObjectById(id);
+        res.status(200).json({ success: true, message: 'Student deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Server Error' });
+    }
+};
+
+module.exports = {
+    getAllStudents,
+    getSingleStudent,
+    createStudentObject,
+    updateStudentObject,
+    deleteStudentObject
 };
--- studentapi/src/v1/Routes/studentRoutes.js
+++ studentapi/src/v1/Routes/studentRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const {getAllStudents, getSingleStudent, createStudentObject, updateStudentObject, deleteStudentObject} = require('../Controllers/studentController');
 
 const router = express.Router();
 
 //GET får å få alle studenter
-router.get('/', );  
-router.get('/:id', ); //GET for å få en student basert på ID, og :id sier at id er en variabel
+router.get('/', getAllStudents);  
+router.get('/:id', getSingleStudent); //GET for å få en student basert på ID, og :id sier at id er en variabel
 
 //POST for å legge til en ny student
-router.post('/', );
-
+router.post('/', createStudentObject);
 //PUT for å oppdatere en student basert på ID
-router.put('/:id', );
+router.put('/:id', updateStudentObject);
 
 //DELETE for å slette en student basert på ID
-router.delete('/:id', );
+router.delete('/:id', deleteStudentObject);
 
 //eksporter routeren, slik at den kan brukes i andre filer
 module.exports = router;
